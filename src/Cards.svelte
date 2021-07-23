92b2fcf9b9add59a873ac75a7a56ca968c15f64c
Odellc <codell03@gmail.com>
2021-07-23 16:28:48 -0600

    update

--- src/Cards.svelte
+++ src/Cards.svelte
@@ -1,40 +1,58 @@
 <script>
-    import {getHref} from "./util/util.js"
-    export let project;
-     
+  import { getHref } from "./util/util.js";
+  export let project;
+
+  let anchor;
+  let h3;
+  let image;
+
+  $: if (image) {
+    console.log(
+      " anchor H3 ParseIntH3",
+      anchor.offsetHeight,
+      h3.offsetHeight,
+      parseInt(window.getComputedStyle(h3).marginBottom)
+    );
+    image.style.height = `${anchor.offsetHeight -
+      h3.offsetHeight -
+      parseInt(window.getComputedStyle(h3).marginBottom)}px`;
+  }
 </script>
 
 <div class="card">
-    <a href={getHref(project.name)}>
-    <h3>
-       {project.name}
-    </h3>
-    <div class="image-class">
-         <img
-            src={project.imageA}
-            alt={project.name}/>
+  <a href={getHref(project.name)} bind:this={anchor}>
+    <h3 bind:this={h3}>{project.name}</h3>
+    <div bind:this={image} class="image-class">
+      <img src={project.imageA} alt={project.name} />
 
     </div>
-    </a>
+  </a>
 </div>
 
 <style>
-    .card{
-        width: 300px;
-        height: 300px;
-        border: 1px solid #aaa;
-        border-radius: 3px;
-        box-shadow: 2px 2px 10px rgba(255, 255, 255);
-        padding: 1em;
-    }
-    h3{
-        padding: 0 0 0.2em 0;
-        margin: 0 0 1em 0;
-        border-bottom: 1px solid #ff3e00;
-    }
+  .card {
+    width: 250px;
+    height: 250px;
+    border: 1px solid #aaa;
+    border-radius: 3px;
+    box-shadow: 2px 2px 10px rgba(255, 255, 255);
+    padding: 1em;
+  }
 
-    :global(.image-class img){
-        width: 150px;
-        height: 150px;
-    }
+  a {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+  }
+
+  h3 {
+    padding: 0 0 0.2em 0;
+    margin: 0 0 1em 0;
+    border-bottom: 1px solid #ff3e00;
+  }
+
+  :global(.image-class img) {
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/Projects.svelte
+++ src/Projects.svelte
@@ -1,67 +1,74 @@
 <script>
-    import About from './About.svelte';
-import App from './App.svelte';
-import Cards from './Cards.svelte'
-    import projects from './pages/projects.js'
-    import skills from './skills.js'
-    
-    export let projectFilter;
+  import About from "./About.svelte";
+  import App from "./App.svelte";
+  import Cards from "./Cards.svelte";
+  import projects from "./pages/projects.js";
+  import skills from "./skills.js";
 
-    $: filteredProjects = projectFilter == null ? projects : projects.filter((project)=>{
-        return project.filter.includes(projectFilter.toLowerCase())
-    })
+  export let projectFilter;
 
-    const filterBySkill = (name) => {
-        projectFilter = name == "All" ? null : name.toLowerCase();
-    };
+  $: filteredProjects =
+    projectFilter == null
+      ? projects
+      : projects.filter(project => {
+          return project.filter.includes(projectFilter.toLowerCase());
+        });
 
-
+  const filterBySkill = name => {
+    projectFilter = name == "All" ? null : name.toLowerCase();
+  };
 </script>
 
 <div class="filter-buttons">
-    {#each [{name: "All"}, ...skills] as skill}
-        <button on:click={()=>filterBySkill(skill.name)} class:selected="{projectFilter==skill.name.toLowerCase() || (projectFilter == null && skill.name == "All")}">{skill.name}</button>
-    {/each}
+  {#each [{ name: 'All' }, ...skills.slice(0, -1)] as skill}
+    <button
+      on:click={() => filterBySkill(skill.name)}
+      class:selected={projectFilter == skill.name.toLowerCase() || (projectFilter == null && skill.name == 'All')}>
+      {skill.name}
+    </button>
+  {/each}
 </div>
-<div>
-{#each filteredProjects as project}
-    
-    <Cards {project}/>
-
-{/each}
+<div class="card-container">
+  {#each filteredProjects as project}
+    <Cards {project} />
+  {/each}
 
 </div>
 
 <style>
+  .filter-buttons {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
 
-    .filter-buttons{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-bottom: 20px;
-    }
+  .filter-buttons button {
+    padding: 10px;
+    overflow: hidden;
+    border: hidden;
+    border-left: 0.5px solid black;
+    border-right: 0.5px solid black;
+    font-weight: 600;
+  }
 
-    .filter-buttons button{
-        padding: 10px;
-        overflow:hidden;
-        border: hidden;
-        border-left: .5px solid black;
-        border-right: .5px solid black;
-        
-    }
+  .filter-buttons button:first-child {
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
 
-    .filter-buttons button:first-child{
-        border-top-left-radius: 5px;
-        border-bottom-left-radius: 5px;
-    }
+  .filter-buttons button:last-child {
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
 
-    .filter-buttons button:last-child{
-        border-top-right-radius: 5px;
-        border-bottom-right-radius: 5px;
-    }
+  .selected {
+    background-color: #99b946;
+  }
 
-    .selected{
-        background-color: #99b946;
-    }
-
+  .card-container {
+    display: flex;
+    flex-direction: row;
+    gap: 25px;
+  }
 </style>
