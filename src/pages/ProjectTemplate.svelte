520d53615dc9baf62c9d95aad18533b95bd8451d
Odellc <codell03@gmail.com>
2021-07-27 16:32:24 -0600

    working on template

--- src/pages/ProjectTemplate.svelte
+++ src/pages/ProjectTemplate.svelte
@@ -1,35 +1,81 @@
 <script>
-    import projects from "./projects.js"
-    import {getHref} from "../util/util.js"
-    import {onMount} from "svelte"
+  import projects from "./projects.js";
+  import { getHref } from "../util/util.js";
+  import { onMount } from "svelte";
 
-    export let params;
-    let projectInfo = {};
+  export let params;
+  let projectInfo = {};
 
-    onMount(async()=>{
-        console.log("Params", params)
+  onMount(async () => {
+    console.log("Params", params);
 
-        for(let i=0; i < projects.length; i++){
-            let name = getHref(projects[i].name).slice(1)
+    for (let i = 0; i < projects.length; i++) {
+      let name = getHref(projects[i].name).slice(1);
 
-            if(params.projectName == name){
-                projectInfo = projects[i];
-            }
-        }
-    });
+      if (params.projectName == name) {
+        projectInfo = projects[i];
+      }
+    }
+  });
 
-    $:console.log("params:", projectInfo)
-
+  $: console.log("params:", projectInfo);
 </script>
 
-<h1>
-    {projectInfo.name}
-</h1>
+<div class="project-template">
+  <h1>{projectInfo.name}</h1>
+
+  <h3>Summary</h3>
+  <div class="intro">
+    <p>{projectInfo.descriptionIntro}</p>
+
+    <img src={projectInfo.imageA} alt="Image One" width="300" height="300" />
+
+  </div>
+
+  <h3>Body</h3>
+  <p>{projectInfo.descriptionBody}</p>
+
+  <img src={projectInfo.imageB} alt="Image Two" width="300" height="300" />
+
+  <h3>Conclusion</h3>
+  <p>{projectInfo.descriptionConclusion}</p>
+
+</div>
 
 <style>
+  .intro {
+    display: inline-block;
+  }
 
-h1{
-color: white;
-}
+  img {
+    float: left;
+    shape-outside: ellipse();
+  }
 
+  .project-template {
+    display: flex;
+    flex-direction: column;
+    padding: 25px;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+    margin-top: 100px;
+  }
+
+  h1 {
+    color: white;
+  }
+
+  h3 {
+    display: flex;
+    width: 100%;
+    color: #99b946;
+    /* text-align: center; */
+    justify-content: center;
+    align-items: center;
+    padding: 0 0 0.4em 0;
+    margin: 0 0 1em 0;
+    border-bottom: 1px solid white;
+    font-size: 2em;
+  }
 </style>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,24 +2,22 @@ export default [
   {
     id: Math.random() * Date.now(),
     name: "Machine Learning",
-    descriptionIntro: '',
+    descriptionIntro:
+      "This is going to be the intro to the woooork I have done. I have no clue of what it will say.This is going to be the intro to the woooork I have done. I have no clue of what it will say.This is going to be the intro to the woooork I have done. I have no clue of what it will say.This is going to be the intro to the woooork I have done. I have no clue of what it will say.This is going to be the intro to the woooork I have done. I have no clue of what it will say.,",
     imageA: "./assets/Python.png",
-    descriptionBody: '',
+    descriptionBody: "",
     imageB: "image 2",
     descriptionConclusion: "",
-    filter: ["python", "machine learning", "d3"],
-
+    filter: ["python", "machine learning", "d3"]
   },
   {
     id: Math.random() * Date.now(),
     name: "Data Mining",
-    descriptionIntro: '',
+    descriptionIntro: "",
     imageA: "./assets/R.jfif",
-    descriptionBody: '',
+    descriptionBody: "",
     imageB: "image 2",
     descriptionConclusion: "",
     filter: ["r", "plotly"]
-
-  },
-  
+  }
 ];
